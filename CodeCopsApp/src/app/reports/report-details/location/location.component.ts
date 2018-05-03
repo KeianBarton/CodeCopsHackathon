e66636dc04978559a243b1767652e7c024924b7e
Keian Barton <keian.barton@civica.co.uk>
2018-05-03 11:08:46 +0100

    Report Details commit

--- CodeCopsApp/src/app/reports/report-details/location/location.component.ts
+++ CodeCopsApp/src/app/reports/report-details/location/location.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-location',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
 
   constructor() { }
 
   ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(51.4594843,-2.5880095),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
 }
