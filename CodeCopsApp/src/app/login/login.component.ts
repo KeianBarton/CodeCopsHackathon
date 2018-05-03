2bff5eaab78b0c97cc92e676d142939d133771bb
Alex <alex.thayne@civica.co.uk>
2018-05-03 17:47:41 +0100

    Fixed login

--- CodeCopsApp/src/app/login/login.component.ts
+++ CodeCopsApp/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from './login.service';
 import { IOfficer } from './IOfficer';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,13 +15,13 @@ export class LoginComponent implements OnInit {
   officers : IOfficer[];
   officerID : number;
 
-  constructor(private loginService: LoginService, private sanitizer: DomSanitizer) { }
+  constructor(private loginService: LoginService, private sanitizer: DomSanitizer, private router : Router) { }
 
   ngOnInit() {
     this.loginService.getOfficers()
       .subscribe(
         response => {
-          this.officers = response;
+          this.officers = response.slice(0,2);
         },
         error => {
           console.log("getOfficers API call failed")
@@ -30,6 +31,10 @@ export class LoginComponent implements OnInit {
 
   officerIdMatches(officerId : number){
     return this.officerID === officerId;
+  }
+
+  Login() {
+    this.router.navigate(['reports']);
   }
 
 }
